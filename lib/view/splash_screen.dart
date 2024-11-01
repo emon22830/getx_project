2aff1f743e7104ccf5c6980e38cc7eca6d1ac592
emon22830 <mdemon22830@gmail.com>
2024-11-01 22:44:54 +0800

    app exceptions

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:getx_project/data/app_exception.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
